eadd94448a279019c36cf3a95c14fd8534bfd271
Conrad Huang <conrad@cgl.ucsf.edu>
2022-06-16 16:19:03 -0700

    Refactor so popup handles only UI and background handles managing downloads

--- background.js
+++ background.js
@@ -1,12 +1,56 @@
-var testData = "Init";
+//
+// vim: set expandtab ts=4 sw=4:
+//
+
 (function() {
-	console.log("init background script: " + testData);
-	browser.runtime.onMessage.addListener((message) => {
-		if (message.command == "test") {
-			console.log("received test: cached data: " + testData);
-			console.log(message);
-			testData = "Changed";
-		} else if (message.command == "") {
-		}
-	});
+
+    // TODO: use local storage so background script need not be persistent
+    var autoresumeIds = ["13"];
+
+    function reloadDownloads() {
+        let query = {"orderBy": ["-startTime"]};
+        let allDownloads = browser.downloads.search(query);
+        function show(dls) {
+            // TODO: remove debug code
+            dls = [{id:12, filename:"hello.png"},
+                   {id:13, filename:"world.gif"}];
+            msg = {command:"show", downloads:dls, auto:autoresumeIds};
+            browser.runtime.sendMessage(msg);
+        }
+        allDownloads.then(show, onError);
+    }
+
+    function onError(error) {
+        console.error("autoresume: " + error.message);
+    }
+
+    // console.log("init background script");
+    browser.runtime.onMessage.addListener((msg) => {
+        console.log("background received command: " + msg.command);
+        console.log(msg);
+        if (msg.command == "popup") {
+            // Send "show" message with latest list of downloads.
+            reloadDownloads();
+        } else if (msg.command == "update") {
+            // Remove download id from autoresume list if not selected.
+            // Add if selected.
+            let n = autoresumeIds.indexOf(msg.id);
+            if (msg.selected) {
+                if (n == -1)
+                    autoresumeIds.push(msg.id);
+            } else {
+                if (n != -1)
+                    autoresumeIds.splice(n, 1);
+            }
+        } else if (msg.command == "finished") {
+            // Remove download id from autoresume list.
+            let n = autoresumeIds.indexOf(msg.id);
+            if (n != -1)
+                autoresumeIds.splice(n, 1);
+        }
+    });
+
+    // TODO: listen for download stopped/paused/failed events
+    // and automatically resume if possible.
+
 })();
--- popup/choose_downloads.js
+++ popup/choose_downloads.js
@@ -1,81 +1,68 @@
-function listenForEvents() {
-	document.addEventListener("click", (e) => {
-		let el = e.target;
-		if (el.classList.contains("autoresume")) {
-			// el.innerHTML = "Hello";
-			let msg = {
-				command: "update",
-				selected: el.checked,
-				id: el.value,
-			};
-			browser.runtime.sendMessage(msg);
-		}
-	});
-}
+//
+// vim: set expandtab ts=4 sw=4:
+//
 
-function reloadDownloads() {
-	let oldDownloads = document.body.querySelectorAll(".autoresume");
-	for (let dl of oldDownloads)
-		dl.remove();
-	let query = {"orderBy": ["-startTime"]};
-	let allDownloads = browser.downloads.search(query);
-	allDownloads.then(showDownloads, onError);
-}
+(function() {
 
-function checkboxClick(ev) {
-	let el = ev.target;
-	console.log(el);
-	// el.innerHTML = "Hello";
-	let msg = {
-		command: "update",
-		selected: el.checked,
-		id: el.value,
-	};
-	browser.runtime.sendMessage(msg);
-	console.log("sent update");
-	console.log(msg);
-}
+    function checkboxClick(ev) {
+        let el = ev.target;
+        // el.innerHTML = "Hello";
+        let msg = {
+            command: "update",
+            selected: el.checked,
+            id: el.value,
+        };
+        browser.runtime.sendMessage(msg);
+        // console.log("sent update");
+        // console.log(msg);
+    }
 
-function showDownloads(downloads) {
-	let activeDownloads = document.body.querySelector(".active-downloads");
-	let count = 0;
-	// TODO: remove debug code
-	downloads = [{id:12, filename:"hello.png"},
-		     {id:13, filename:"world.gif"}];
-	for (let dl of downloads) {
-		console.log(dl);
-		let dlId = dl.id.toString();
-		// If download is finished, we should not display it.
-		if (dl.state == "complete") {
-			browser.runtime.sendMessage({command:"finished",
-						     id:dlId});
-			continue;
-		}
-		let checkbox = document.createElement("input");
-		checkbox.setAttribute("type", "checkbox");
-		checkbox.value = dlId;
-		checkbox.className = "autoresume";
-		// TODO: need to figure out whether box should be checked
-		checkbox.addEventListener("change", checkboxClick);
-		let label = document.createElement("label");
-		label.innerHTML = dl.filename.replace(/^.*[\\\/]/, '');
-		let br = document.createElement("br");
-		activeDownloads.appendChild(checkbox);
-		activeDownloads.appendChild(label);
-		activeDownloads.appendChild(br);
-		count += 1;
-	}
-	if (count == 0) {
-		let p = document.createElement("p");
-		p.innerHTML = "No active downloads.";
-		// p.className = "autoresume";
-		activeDownloads.appendChild(p);
-	}
-}
+    function showDownloads(downloads, auto) {
+        let oldDownloads = document.body.querySelectorAll(".autoresume");
+        for (let dl of oldDownloads)
+            dl.remove();
+        let activeDownloads = document.body.querySelector(".active-downloads");
+        let count = 0;
+        for (let dl of downloads) {
+            console.log(dl);
+            let dlId = dl.id.toString();
+            // If download is finished, we should not display it.
+            if (dl.state == "complete") {
+                browser.runtime.sendMessage({command:"finished",
+                                 id:dlId});
+                continue;
+            }
+            let checkbox = document.createElement("input");
+            checkbox.setAttribute("type", "checkbox");
+            checkbox.value = dlId;
+            checkbox.className = "autoresume";
+            if (auto.indexOf(dlId) != -1)
+                checkbox.checked = true;
+            checkbox.addEventListener("change", checkboxClick);
+            let label = document.createElement("label");
+            label.innerHTML = dl.filename.replace(/^.*[\\\/]/, '');
+            let br = document.createElement("br");
+            activeDownloads.appendChild(checkbox);
+            activeDownloads.appendChild(label);
+            activeDownloads.appendChild(br);
+            count += 1;
+        }
+        if (count == 0) {
+            let p = document.createElement("p");
+            p.innerHTML = "No active downloads.";
+            // p.className = "autoresume";
+            activeDownloads.appendChild(p);
+        }
+    }
 
-function onError(error) {
-	console.error(`autoresume: ${error.message}`);
-}
+    // console.log("init popup script");
+    browser.runtime.onMessage.addListener((msg) => {
+        console.log("popup received command: " + msg.command);
+        console.log(msg);
+        if (msg.command == "show") {
+            showDownloads(msg.downloads, msg.auto);
+        }
+    });
+    browser.runtime.sendMessage({command:"popup"});
 
-listenForEvents();
-reloadDownloads();
+})();
